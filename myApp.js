e55edd4e39259de4d2a8a541a06e8b2f24e8e3ab
Zain <101869234+Zainkiani247@users.noreply.github.com>
2023-02-03 06:51:18 +0100

    added packages and html file send

--- myApp.js
+++ myApp.js
@@ -2,7 +2,12 @@ let express = require("express");
 let app = express();
 
 console.log("Hello World");
+// app.get("/", (req, res) => {
+//   res.send("Hello Express");
+// });
+
 app.get("/", (req, res) => {
-    res.send("Hello Express");
-  });
+  res.sendFile(__dirname + "/views/index.html");
+});
+
 module.exports = app;
