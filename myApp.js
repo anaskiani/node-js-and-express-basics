77a4f6c36fce23e4f5bb1efdc1a20fb005942b78
Zain <101869234+Zainkiani247@users.noreply.github.com>
2023-02-06 07:18:23 +0100

    key  correction

--- myApp.js
+++ myApp.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.json({ "message": "Hello json" });
+  res.json({ message: "Hello json" });
 });
 
 module.exports = app;
