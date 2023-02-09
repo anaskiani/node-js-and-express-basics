1687736223f02751b80e76b0d7bb94152d98f156
Zain <101869234+Zainkiani247@users.noreply.github.com>
2023-02-09 07:27:04 +0100

    used app.get with arrow

--- myApp.js
+++ myApp.js
@@ -51,15 +51,14 @@ app.get(
 );
 app.get("/:word/echo", (req, res) => {
   const { word } = req.params;
-res.json({
-  echo: word
-});
-});
-app.route('/name')
-  .get((req, res) => {
-    const { first, last } = req.query;
-    const name = `${first} ${last}`;
-    res.json({ name });
+  res.json({
+    echo: word,
   });
+});
+app.get("/name", (req, res) => {
+  const { first, last } = req.query;
+  const name = `${first} ${last}`;
+  res.json({ name });
+});
 
 module.exports = app;
